c1be472a4d59c36aa9d33f1027d9645eee188ff8
Enercoling <famousman692@gmail.com>
2025-06-13 23:18:05 +0200

    Hyprland config

--- config/nvim/lua/config/keybinds.lua
+++ config/nvim/lua/config/keybinds.lua
@@ -6,4 +6,4 @@ m("n", "<C-tab>", ":Neotree buffers<Enter>")
 
 m("n", "<leader>bd", ":DBUIToggle<Enter>")
 
-m("n", "<C-j>", ":belowright 10split | terminal<CR>")
+m("n", "<C-j>", ":bel 10split | term<CR>")
